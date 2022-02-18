63ac7ba840e5a912b26f39b7169ae2cbeafcca01
SyedJunaidNisar <syedjunaidnisar11@gmail.com>
2022-02-17 23:39:21 -0800

    header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,31 @@
+import { Link } from "gatsby"
+import React from "react"
+import styled from "styled-components"
+
+const menuData = [
+  { title: "Courses", icon: "/images/icons/courses.svg", link: "/courses" },
+  {
+    title: "Tutorials",
+    icon: "/images/icons/tutorials.svg",
+    link: "/tutorials",
+  },
+  { title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
+]
+
+export default function Header() {
+  return (
+    <Wrapper>
+        <img src="/images/logos/logo.svg" />
+      {menuData.map((item, index) => (
+        <Link to={item.link} key={index}> 
+          <img src="item.icon" alt="item.title" />
+          {item.title}
+        </Link>
+      ))}
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+
+`
