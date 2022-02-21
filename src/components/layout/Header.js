9c73c3cc5994aee219828788baa8dfdf6cd8cc93
SyedJunaidNisar <syedjunaidnisar11@gmail.com>
2022-02-20 17:18:36 -0800

    UI changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,20 +2,18 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import { menuData } from "../../data/menuData"
+import MenuButton from "../buttons/MenuButton"
 
 export default function Header() {
   return (
     <Wrapper>
-        <img src="/images/logos/logo.svg" />
-        <MenuWrapper>
-      {menuData.map((item, index) => (
-        <Link to={item.link} key={index}> 
-        <MenuItem>
-          <img src={item.icon} alt={item.title} />
-          {item.title}
-          </MenuItem>
-        </Link>
-      ))}
+      <Link to="/">
+      <img src="/images/logos/logo.svg" />
+      </Link>
+      <MenuWrapper count={menuData.length}>
+        {menuData.map((item, index) => (
+          <MenuButton item={item} key={index} />
+        ))}
       </MenuWrapper>
     </Wrapper>
   )
@@ -34,23 +32,5 @@ const Wrapper = styled.div`
 const MenuWrapper = styled.div`
   display: grid;
   gap: 30px;
-  grid-template-columns: repeat(5, auto);
+  grid-template-columns: repeat(${props => props.count}, auto);
 `
-
-const MenuItem = styled.div`
-  color: rgba(255, 255, 255, 0.7);
-  display: grid;
-  grid-template-columns: 24px auto;
-  gap: 10px;
-  align-items: center;
-  padding: 10px;
-  border-radius: 10px;
-  transition: 0.5s ease-out;
-
-  :hover {
-    background: rgba(255, 255, 255, 0.1);
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
-    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
-  }
-`
-
